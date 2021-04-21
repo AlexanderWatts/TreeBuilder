79b26b386fceedf26677cf4eed39e695421c3a72
Alexander Watts <acwatts.dev@gmail.com>
2021-04-21 12:26:30 +0100

    Wrapped components in current tree provider to access the current rendered tree state

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { TreeHeightContextProvider } from './contexts/TreeHeightContext';
+import { CurrentTreeContextProvider } from './contexts/CurrentTreeContext';
 import Controls from './components/Controls';
 import Tree from './components/Tree';
+import TreeStats from './components/TreeStats';
 
 const App = () => (
   <>
     <TreeHeightContextProvider>
-      <Controls />
-      <Tree />
+      <CurrentTreeContextProvider>
+        <Controls />
+        <Tree />
+        <TreeStats />
+      </CurrentTreeContextProvider>
     </TreeHeightContextProvider>
   </>
 );
