a73cf62b77dcba4d27b602c988f8a3318a97b385
Alexander Watts <acwatts.dev@gmail.com>
2021-04-22 17:56:27 +0100

    Created algorithm for sorting tree data given a sorted array

---
+++ src/sortTree.js
@@ -0,0 +1,20 @@
+import node from './node';
+
+const sortBalancedTree = (array, start, end) => {
+  if (end < start) {
+    return null;
+  }
+  const mid = Math.ceil((start + end) / 2);
+  const root = node();
+  root.data = array[mid];
+  root.label = `${array[mid]}`;
+
+  // Conditional prevents creating extra empty nodes
+  if (end > start) {
+    root.children.push(sortBalancedTree(array, start, mid - 1));
+    root.children.push(sortBalancedTree(array, mid + 1, end));
+  }
+  return root;
+};
+
+export default sortBalancedTree;
