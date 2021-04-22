6c285b484b9e57fd3691d5c2624baec1b25902be
Alexander Watts <acwatts.dev@gmail.com>
2021-04-22 17:58:52 +0100

    Created new tree height controller that allows the user to change the height of a tree and removed the old controller

---
+++ src/components/TreeHeightController/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.css';
+
+const TreeHeightController = ({ height, setHeight }) => {
+  const updateTreeHeight = (event) => {
+    const treeHeight = event.target.value;
+    if (treeHeight >= 1 && treeHeight <= 10) {
+      setHeight(treeHeight);
+    }
+  };
+
+  return (
+    <div className="heightController">
+      <label htmlFor="height">
+        <span>Set tree height:</span>
+        <input id="height" type="number" value={height} onChange={updateTreeHeight} />
+      </label>
+    </div>
+  );
+};
+
+TreeHeightController.propTypes = {
+  height: PropTypes.number.isRequired,
+  setHeight: PropTypes.func.isRequired,
+};
+
+export default TreeHeightController;
